080779888f42772e71ab7d6fc0544c5341474129
KimJin95 <129369841+KimJin95@users.noreply.github.com>
2023-05-17 16:41:08 +0900

    shoot

--- Assets/02.Scripts/Gun.cs
+++ Assets/02.Scripts/Gun.cs
@@ -4,5 +4,43 @@ using System.Collections;
 // 필요 속성: 총알 파편, 총알 파편 효과, 총알 발사 사운드
 public class Gun : MonoBehaviour
 {
-    
+    [SerializeField] Transform crossHair;
+    [SerializeField] ParticleSystem bulletEffect;
+    [SerializeField] AudioSource bulletAudio;
+
+
+    private void Update()
+    {
+        ARAVRInput.DrawCrosshair(crossHair);
+
+        shot();
+    }
+
+    void shot()
+    {
+        if (ARAVRInput.GetDown(ARAVRInput.Button.IndexTrigger))
+        {
+           
+
+            bulletAudio.Stop();
+            bulletAudio.Play();
+
+            Ray ray = new Ray(ARAVRInput.RHandPosition, ARAVRInput.RHandDirection);
+            RaycastHit hit;
+
+            int playerLayer = 1 << LayerMask.NameToLayer("Player");
+            int towerLayer = 1 << LayerMask.NameToLayer("Tower");
+            int layerMask = playerLayer | towerLayer;
+
+            if (Physics.Raycast(ray, out hit, 200,~layerMask))
+            {
+
+                bulletEffect.Stop();
+                bulletEffect.Play();
+
+                bulletEffect.transform.position = hit.point;
+                bulletEffect.transform.forward = hit.normal;
+            }
+        }
+    }
 }
--- Assets/02.Scripts/TeleportStraight.cs
+++ Assets/02.Scripts/TeleportStraight.cs
@@ -5,5 +5,48 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class TeleportStraight : MonoBehaviour
 {
-    
+    [SerializeField] Transform teleportCircle;
+
+    LineRenderer myline;
+    Vector3 originScale = Vector3.one * 0.02f;
+
+    private void Start()
+    {
+        teleportCircle.gameObject.SetActive(false);
+        myline = GetComponent<LineRenderer>();
+        myline.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (ARAVRInput.GetDown(ARAVRInput.Button.One, ARAVRInput.Controller.LTouch))
+        {
+            myline.enabled = true;
+        }
+
+        else if (ARAVRInput.GetUp(ARAVRInput.Button.One, ARAVRInput.Controller.LTouch))
+        {
+            myline.enabled = false;
+            teleportCircle.gameObject.SetActive(false);
+        }
+
+        else if (ARAVRInput.Get(ARAVRInput.Button.One, ARAVRInput.Controller.LTouch))
+        {
+            Ray ray = new Ray(ARAVRInput.LHandPosition, ARAVRInput.LHandDirection);
+            RaycastHit hit;
+
+            int layerMask = 1 << LayerMask.NameToLayer("Terrain");
+            if (Physics.Raycast(ray, out hit, 200, layerMask))
+            {
+                myline.SetPosition(0, ray.origin);
+                myline.SetPosition(0, hit.point);
+
+                teleportCircle.gameObject.SetActive(true);
+                teleportCircle.position = hit.point;
+                teleportCircle.forward = hit.normal;
+                teleportCircle.localScale = originScale * Mathf.Max(1, hit.distance);
+            }
+
+        }
+    }
 }
