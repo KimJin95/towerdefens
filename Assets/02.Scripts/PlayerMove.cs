0d195effd2281963d5afa296641df58f04aa1a31
KimJin95 <129369841+KimJin95@users.noreply.github.com>
2023-05-17 15:23:50 +0900

    playerMove
    이동 및 점프

--- Assets/02.Scripts/PlayerMove.cs
+++ Assets/02.Scripts/PlayerMove.cs
@@ -2,7 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//사용자의 입력에 따라 4방으로 이동처리
+//중력 적용
+//점프(오른쪽 패드 클릭)
+//카메라가 바라보는 방향으로 이동
+
 public class PlayerMove : MonoBehaviour
 {
-    
+    [SerializeField] float speed = 3;
+    [SerializeField] float jumpPower = 5;
+    [SerializeField] float gravity = -20f;
+
+    float velocityY;
+
+    CharacterController mycontroller;
+
+    private void Start()
+    {
+        mycontroller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float h = ARAVRInput.GetAxis("Horizontal");
+        float v = ARAVRInput.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+        dir = Camera.main.transform.TransformDirection(dir);
+
+        velocityY += gravity * Time.deltaTime;
+        if (mycontroller.isGrounded) velocityY = 0;
+
+        if (ARAVRInput.GetDown(ARAVRInput.Button.Two)) velocityY = jumpPower;
+        dir.y = velocityY;
+
+        mycontroller.Move(dir * speed * Time.deltaTime);
+    }
 }
